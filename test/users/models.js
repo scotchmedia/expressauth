fb344a0d2cc29e88cc80292a6721daa27754f22d
Kyle Finley <kyle.finley@gmail.com>
2012-10-09 03:39:01 -0500

    Add fields to the User model
    First modify the test:

--- test/users/models.js
+++ test/users/models.js
@@ -16,14 +16,29 @@ describe('Users: models', function () {
         name: {
           givenName: 'Barack',
           familyName: 'Obama'
-        }
+        },
+        emails: [
+          {
+            type: 'home',
+            value: 'home@example.com'
+          },
+          {
+            type: 'work',
+            value: 'work@example.com'
+          }
+        ]
       };
-      User.create([u], function (err, createdUser) {
+      User.create(u, function (err, createdUser) {
         // Confirm that that an error does not exist
         should.not.exist(err);
         // verify that the returned user is what we expect
         createdUser.name.givenName.should.equal('Barack');
         createdUser.name.familyName.should.equal('Obama');
+        // new tests
+        createdUser.emails[0].type.should.equal('home');
+        createdUser.emails[0].value.should.equal('home@example.com');
+        createdUser.emails[1].type.should.equal('work');
+        createdUser.emails[1].value.should.equal('work@example.com');
         // Call done to tell mocha that we are done with this test
         done();
       });
