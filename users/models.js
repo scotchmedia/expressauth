f6fc31aef8fd1f36c75b14f88d7329868c353e4c
Kyle Finley <kyle.finley@gmail.com>
2012-10-09 03:39:29 -0500

    Add an emails list to the User model
    Test should now be passing. If they are not try restarting the test
       runner. `command+c` and running `npm start`

--- users/models.js
+++ users/models.js
@@ -3,12 +3,22 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// define the emailSchema
+var emailSchema = new Schema({
+  // Since `type` is special keyword in mongoose we must set the def. to
+  // and object. I.e. this would not work:
+  // type: String,
+  type  : {type: String},
+  value : String
+});
+
 // define the userSchema
 var userSchema = new Schema({
   name  : {
     givenName   : String,
     familyName  : String
-  }
+  },
+  emails: [emailSchema]
 });
 
 // Export the User model
