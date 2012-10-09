f66783efbc10f962df0ee88555974da2496cdc66
Kyle Finley <kyle.finley@gmail.com>
2012-10-09 01:18:52 -0500

    Create a config file for global settings. Add a db key, with keys for production, development and test. Doing this allows us to set different databases for different environments.
    config.js
    
    module.exports = {
     db: {
       production: "mongodb://user:pass@example.com:1234/stroeski-prod",
       development: "mongodb://localhost/storeski-dev",
       test: "mongodb://localhost/storski-test",
     }
    };

---
+++ config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  db: {
+    production: "mongodb://user:pass@example.com:1234/stroeski-prod",
+    development: "mongodb://localhost/storeski-dev",
+    test: "mongodb://localhost/storski-test",
+  }
+};
