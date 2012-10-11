80877a76c3ea183339c2c2750f754ead94e4c313
Kyle Finley <kyle.finley@gmail.com>
2012-10-11 01:20:18 -0500

    Write your first acceptence test:
    To go along with our first unit test we will will create an route that does addtion. Give the route /add/1/1 we want to return 2.

--- test/addition.js
+++ test/addition.js
@@ -1,5 +1,12 @@
 'use strict';
 
+// import the moongoose helper utilites
+var utils = require('./utils');
+var request = require('supertest');
+var should = require('should');
+var app = require('../app').app;
+
+
 describe('addition', function () {
   it('should add 1+1 correctly', function (done) {
     var onePlusOne = 1 + 1;
@@ -7,5 +14,15 @@ describe('addition', function () {
     // must call done() so that mocha know that we are... done.
     // Useful for async tests.
     done();
+  });
+  it('should return 2 given the url /add/1/1', function (done) {
+    request(app)
+      .get('/add/1/1')
+      .expect(200)
+      .end(function (err, res) {
+        should.not.exist(err);
+        parseFloat(res.text).should.equal(2);
+        done();
+      });
   });
 });
