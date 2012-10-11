3317e60691da7a17538cabb58cbd643af5ed2165
Kyle Finley <kyle.finley@gmail.com>
2012-10-11 16:34:04 -0500

    Write tests to check validation

--- test/users/routes.js
+++ test/users/routes.js
@@ -9,11 +9,11 @@ var app = require('../../app').app;
 
 describe('Users: routes', function () {
   describe('POST /signup', function () {
-    it('should redirect to "/" if the form is valid', function (done) {
+    it('should redirect to "/account" if the form is valid', function (done) {
       var post = {
         givenName: 'Barrack',
         familyName: 'Obama',
-        username: 'berry',
+        email: 'berry@example.com',
         password: 'secret'
       };
       request(app)
@@ -24,6 +24,24 @@ describe('Users: routes', function () {
           should.not.exist(err);
           // confirm the redirect
           res.header.location.should.include('/account');
+          done();
+        });
+    });
+    it('should redirect to "/login" if the form is invalid', function (done) {
+      var post = {
+        givenName: 'Barrack',
+        familyName: '',
+        email: 'fakeemail',
+        password: 'se'
+      };
+      request(app)
+        .post('/signup')
+        .send(post)
+        .expect(302)
+        .end(function (err, res) {
+          should.not.exist(err);
+          // confirm the redirect
+          res.header.location.should.include('/signup');
           done();
         });
     });
