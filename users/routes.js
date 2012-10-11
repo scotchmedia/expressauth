b3ce0498f760c779a3ff445b670c48f95361b99c
Kyle Finley <kyle.finley@gmail.com>
2012-10-11 16:34:23 -0500

    Write code to make it pass

--- users/routes.js
+++ users/routes.js
@@ -7,5 +7,13 @@ var User = require('./models').User;
  */
 
 exports.signup = function (req, res) {
+  req.onValidationError(function (msg) {
+    //Redirect to `/signup` if validation fails
+    return res.redirect('/signup');
+  });
+  req.check('email', 'Please enter a valid email').len(1).isEmail();
+  req.check('password', 'Please enter a password with a length between 4 and 34 digits').len(4, 34);
+  req.check('givenName', 'Please enter your first name').len(1);
+  req.check('familyName', 'Please enter your last name').len(1);
   res.redirect('/account');
 };
